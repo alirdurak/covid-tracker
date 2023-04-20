514c25770092d6bb7bc172661375104d1f8f3766
alirdurak <durakaliriza17@gmail.com>
2023-04-20 12:46:24 +0300

    header component logo change

--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,17 @@
 import styles from "./styles.module.scss";
-import { TbVirusSearch, TbMoon } from "react-icons/tb";
+import Image from "next/image";
+import { TbMoon } from "react-icons/tb";
+import covidLogo from "@/public/covid-logo.png";
+
 export default function Header() {
   return (
     <header className={styles.header}>
-      <TbVirusSearch className={styles.virus} />
+      <Image
+        src={covidLogo}
+        width={75}
+        height={75}
+        alt="This is the last pandemic purpose!"
+      />
       <TbMoon className={styles.darkmode} />
     </header>
   );
