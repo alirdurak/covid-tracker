b517c81023b2b8e375521dffd88edc99fe142ec9
alirdurak <durakaliriza17@gmail.com>
2023-04-20 15:21:41 +0300

    layout changes

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,11 +1,12 @@
+import styles from "./styles.module.scss";
 import Header from "../header";
 import Footer from "../footer";
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
       <main>{children}</main>
-      <Footer/>
-    </>
+      <Footer />
+    </div>
   );
 }
