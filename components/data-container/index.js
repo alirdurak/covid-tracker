627af3658ff304189e5d7eeb843b6a68bf2ce3d4
alirdurak <durakaliriza17@gmail.com>
2023-05-17 20:47:52 +0300

    single data showed

--- components/data-container/index.js
+++ components/data-container/index.js
@@ -1,12 +1,15 @@
 import Card from "../card";
 import styles from "./style.module.scss";
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContinentsData, fetchCountriesData } from "@/services";
 export default function DataContainer() {
   const dispatch = useDispatch();
   const dataType = useSelector((state) => state.covidSlice.dataType);
   const fetchStatus = useSelector((state) => state.covidSlice.dataStatus);
+  const location = useSelector((state) => state.covidSlice.locationValue);
+  const data = useSelector((state) => state.covidSlice.data);
+  // Data fetch options
   useEffect(() => {
     if (dataType === "Countries") {
       dispatch(fetchCountriesData());
@@ -14,11 +17,35 @@ export default function DataContainer() {
       dispatch(fetchContinentsData());
     }
   }, [dataType]);
+
+  // Showed data
+  const showedData = data.filter((item) => {
+    if (dataType === "Countries") {
+      return item.country === location;
+    } else if (dataType === "Continents") {
+      return item.continent === location;
+    } else {
+      return null;
+    }
+  });
+  console.log(showedData);
   return (
     <div className={styles.container}>
-      <Card status="case" fetchStatus={fetchStatus} />
-      <Card status="recovered" fetchStatus={fetchStatus} />
-      <Card status="death" fetchStatus={fetchStatus} />
+      <Card
+        status="case"
+        fetchStatus={fetchStatus}
+        data={showedData[0] ? showedData[0].totalCases : NaN}
+      />
+      <Card
+        status="recovered"
+        fetchStatus={fetchStatus}
+        data={showedData[0] ? showedData[0].totalRecovered : NaN}
+      />
+      <Card
+        status="death"
+        fetchStatus={fetchStatus}
+        data={showedData[0] ? showedData[0].totalDeaths : NaN}
+      />
     </div>
   );
 }
